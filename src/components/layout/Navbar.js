db9ff6d5cd99349b63f94a3a611731f59ed650a5
sourav kumar <sourav.baranwal@gmail.com>
2020-04-14 17:51:33 +0530

    nav updated with login and logout logic

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -3,7 +3,9 @@ import { Link } from "react-router-dom";
 import SignedInLinks from "./SignedInLinks";
 import SignedOutLinks from "./SignedOutLinks";
 import { connect } from "react-redux";
-const Navbar = () => {
+const Navbar = (props) => {
+  const { auth } = props;
+
   return (
     <>
       <nav className="nav-wrapper grey darken-3">
@@ -11,8 +13,7 @@ const Navbar = () => {
           <Link to="/" className="brand-logo">
             Planning App
           </Link>
-          <SignedInLinks />
-          <SignedOutLinks />
+          {auth.uid ? <SignedInLinks /> : <SignedOutLinks />}
         </div>
       </nav>
     </>
@@ -21,7 +22,9 @@ const Navbar = () => {
 
 const mapStateToProps = (state) => {
   console.log(state);
-  return {};
+  return {
+    auth: state.firebase.auth,
+  };
 };
 
 export default connect(mapStateToProps)(Navbar);
